b9b0f60a155904c4a472dc59014a57b5ed4684ab
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-22 21:20:34 +0100

    new changes

--- src/main.js
+++ src/main.js
@@ -12,9 +12,10 @@ import TeamMembers from './components/teams/TeamMembers'
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        { path: '/', redirect: '/teams' },
         { path: '/teams', component: TeamsList },
         { path: '/users', component: UsersList },
-        { path: '/teams/:teamId', component: TeamMembers }
+        { path: '/teams/:teamId', component: TeamMembers, props: true }
     ],
     linkActiveClass: 'active-link'
 });
