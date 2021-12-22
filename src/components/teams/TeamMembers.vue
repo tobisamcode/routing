7e175ee3ce2b9e275db79ce8fe46d4c1f2f193eb
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-22 21:20:38 +0100

    new changes

--- src/components/teams/TeamMembers.vue
+++ src/components/teams/TeamMembers.vue
@@ -9,6 +9,7 @@
         :role="member.role"
       ></user-item>
     </ul>
+    <router-link to="/teams/t2">Go to Team 2</router-link>
   </section>
 </template>
 
@@ -17,6 +18,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['users', 'teams'],
+  props: ['teamId'],
   components: {
     UserItem
   },
@@ -26,18 +28,27 @@ export default {
       members: []
     };
   },
+  methods: {
+    loadTeamMembers(teamId){
+      const selectedTeam = this.teams.find(team => team.id === teamId);
+      const members = selectedTeam.members;
+      const selectedMembers = [];
+      for (const member of members) {
+        const selectedUser = this.users.find(user => user.id === member);
+        selectedMembers.push(selectedUser)
+      } 
+      this.members = selectedMembers;
+      this.teamName = selectedTeam.name;
+    }
+  },
   created(){
-    // this.$route.path
-    const teamId = this.$route.params.teamId;
-    const selectedTeam = this.teams.find(team => team.id === teamId);
-    const members = selectedTeam.members;
-    const selectedMembers = [];
-    for (const member of members) {
-      const selectedUser = this.users.find(user => user.id === member);
-      selectedMembers.push(selectedUser)
-    } 
-    this.members = selectedMembers;
-    this.teamName = selectedTeam.name;
+    // this.$route.pa th
+    this.loadTeamMembers(this.teamId)
+  },
+  watch:{
+    teamId(newId) {
+      this.loadTeamMembers(newId);
+    }
   }
 };
 </script>
