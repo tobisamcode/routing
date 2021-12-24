53b20fe44ecd7e9b1218b07c1198473da47cfe11
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-24 18:04:49 +0100

    utilizing route metadata

--- src/main.js
+++ src/main.js
@@ -19,6 +19,7 @@ const router = createRouter({
         {
             name: 'teams',
             path: '/teams',
+            meta: { needsAuth: true },
             components: { default: TeamsList, footer: TeamsFooter },
             children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
@@ -44,6 +45,13 @@ const router = createRouter({
 router.beforeEach(function(to, from, next) {
     console.log('Global beforeEach')
     console.log(to, from);
+
+    if (to.meta.needsAuth) {
+        console.log('Needs auth!');
+        next()
+    } else {
+        next()
+    }
     // if (to.name === 'team-members') {
     //     next()
     // } else {
