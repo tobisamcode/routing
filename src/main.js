b7dfd24846062fe6b5b53cc98e94fd90bf94fe24
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-24 12:54:22 +0100

    users and teams footer added

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter';
+import UsersFooter from './components/users/UsersFooter';
 
 
 const router = createRouter({
@@ -17,12 +19,15 @@ const router = createRouter({
         {
             name: 'teams',
             path: '/teams',
-            component: TeamsList,
+            components: { default: TeamsList, footer: TeamsFooter },
             children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
             ]
         },
-        { path: '/users', component: UsersList },
+        {
+            path: '/users',
+            components: { default: UsersList, footer: UsersFooter }
+        },
 
         { path: '/:notFound(.*)', component: NotFound }
     ],
