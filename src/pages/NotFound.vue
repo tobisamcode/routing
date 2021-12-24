8fd4d478956aa74ac94475c4ea23e1820404a28e
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-12-24 18:14:26 +0100

    organizing routes file

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>
+        Page not found! Do you mean 
+        <router-link to="/teams">Teams</router-link>?
+    </h2>
+</template>
+
+
+<style scoped>
+h2{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
